b6317a1c8580a5d7757265ce6165e477fcd1366b
droberts18 <droberts@Drews-MacBook-Pro.local>
2016-03-30 13:03:15 -0700

    Potentially created a list view?
    Android Emulator is too slow to test

--- LoCal/app/src/main/java/com/example/local/local/MainActivity.java
+++ LoCal/app/src/main/java/com/example/local/local/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.local.local;
 
+import android.app.usage.UsageEvents;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.*;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView calendarView = (ListView) findViewById(R.id.calendarView);
+
+        String[] events = new String[]{"Mobile Applications Development", "Artificial Intelligence",
+        "Psychology", "Lunch", "Dinner"};
+
+        final ArrayList<String> EventsList = new ArrayList<String>();
+        for(int i = 0; i < events.length; i++){
+            EventsList.add(events[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, EventsList);
+        calendarView.setAdapter(adapter);
     }
 }
